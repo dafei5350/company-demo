e8adab2f2a1da5e0cda98c0e795a8938bf663c0d
weiliang <535027650@qq.com>
2021-05-28 14:56:44 +0800

    自定义引入组件

--- src/main.js
+++ src/main.js
@@ -1,15 +1,40 @@
 import { createApp } from 'vue'
-
 import router from '@/router'
-import { ElButton } from 'element-plus'
 import App from './App.vue'
 
 
-// import 'element-plus/lib/theme-chalk/index.css';
+
+import { 
+  ElHeader,
+  ElMain,
+  ElContainer,
+  ElFooter, 
+  ElButton,
+  ElInput,
+  ElCarousel,
+  ElCarouselItem,
+} from 'element-plus'
+
+const components = [
+  ElHeader,
+  ElMain,
+  ElContainer,
+  ElFooter, 
+  ElButton,
+  ElInput,
+  ElCarousel,
+  ElCarouselItem,
+]
+const plugins = [
+
+]
+
+
 // 自定义主题
+// import 'element-plus/lib/theme-chalk/index.css';
 import '~/theme/index.css'
 // 基于断点的隐藏类
-import 'element-plus/lib/theme-chalk/display.css';
+// import 'element-plus/lib/theme-chalk/display.css';
 
 // createApp(App).use(router, ElementPlus).mount('#app')
 
@@ -17,6 +42,10 @@ const app = createApp(App)
 
 app.use(router)
 
-app.use(ElButton)
-
+components.forEach(component => {
+  app.component(component.name, component)
+})
+plugins.forEach(plugin => {
+  app.use(plugin)
+})
 app.mount('#app')
